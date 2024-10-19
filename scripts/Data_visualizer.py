6a765880fdb9fd924e9f24396e0c39b1a4b40dd4
getahunTiruneh <gechtiru@gmail.com>
2024-10-19 12:45:08 +0300

    finalize data visualizer script

--- scripts/Data_visualizer.py
+++ scripts/Data_visualizer.py
@@ -3,17 +3,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import logging as logg
+
 logg.basicConfig(level=logg.INFO)
 
 class DataVisualizer:
+    """
+    A class for visualizing data using various plotting techniques.
+
+    Attributes:
+        data (pd.DataFrame): The input DataFrame containing the dataset to be visualized.
+    """
+
     def __init__(self, data):
+        """
+        Initializes the DataVisualizer with the provided dataset.
+
+        Args:
+            data (pd.DataFrame): The dataset to visualize.
+        """
         self.data = data
+
     def visualize_data(self):
-        # Visualizing the data
+        """Visualizes the data using a pairplot to show relationships between features."""
         sns.pairplot(self.data)
         plt.show()
-    
+
     def plot_histogram(self, numerical_features):
+        """Plots histograms for the specified numerical features.
+
+        Args:
+            numerical_features (list): List of numerical feature names to plot.
+        """
         plt.figure(figsize=(16, 5))
         for i, feature in enumerate(numerical_features, 1):
             plt.subplot(1, len(numerical_features), i)
@@ -24,24 +44,24 @@ class DataVisualizer:
         plt.tight_layout()
         plt.show()
         logg.info("Histograms plotted successfully!")
+
     def plot_bar_chart(self, categorical_features: list):
-        """Plot bar charts for each categorical feature in 2 columns.
+        """Plots bar charts for each specified categorical feature in a grid layout.
 
         Args:
-            categorical_features (list): List of categorical feature names.
+            categorical_features (list): List of categorical feature names to plot.
         """
         try:
-            # Set up number of rows and columns (2 columns max)
             num_features = len(categorical_features)
             num_cols = 2  # We want 2 columns
-            num_rows = (num_features + num_cols - 1) // num_cols  # Calculate rows dynamically
+            num_rows = (num_features + num_cols - 1) // num_cols
             
-            plt.figure(figsize=(num_cols * 6, num_rows * 4))  # Adjust the figure size based on columns and rows
+            plt.figure(figsize=(num_cols * 6, num_rows * 4))
 
             for i, feature in enumerate(categorical_features, 1):
                 if feature not in self.data.columns:
                     logg.error(f"Feature '{feature}' not found in data!")
-                    continue  # Skip this feature if it's not in the data
+                    continue
 
                 plt.subplot(num_rows, num_cols, i)
                 sns.barplot(
@@ -60,16 +80,26 @@ class DataVisualizer:
         except Exception as e:
             logg.error(f"An error occurred while plotting bar charts: {e}")
 
-    
     def plot_scatter_matrix(self, numerical_features):
+        """Plots a scatter matrix for the specified numerical features.
+
+        Args:
+            numerical_features (list): List of numerical feature names to plot.
+        """
         plt.figure(figsize=(16, 10))
         sns.pairplot(self.data[numerical_features], palette='viridis')
         plt.title('Scatter Matrix')
         plt.tight_layout()
         plt.show()
         logg.info("Scatter matrix plotted successfully!")
-        
+
     def scatter_plot(self, x_feature, y_feature):
+        """Plots a scatter plot for two specified features.
+
+        Args:
+            x_feature (str): The feature for the x-axis.
+            y_feature (str): The feature for the y-axis.
+        """
         logg.info("Plotting scatter plot...")
         plt.figure(figsize=(8, 6))
         sns.scatterplot(x=self.data[x_feature], y=self.data[y_feature], palette='viridis')
@@ -77,53 +107,61 @@ class DataVisualizer:
         plt.xlabel(x_feature)
         plt.ylabel(y_feature)
         plt.show()
-        
+
     def plot_box_plot(self, numerical_features):
+        """Plots box plots for the specified numerical features.
+
+        Args:
+            numerical_features (list): List of numerical feature names to plot.
+        """
         plt.figure(figsize=(16, 5))
         for i, feature in enumerate(numerical_features, 1):
             plt.subplot(1, len(numerical_features), i)
             sns.boxplot(self.data[feature])
             plt.title(f'Box Plot for {feature}')
             plt.xlabel(feature)
-    
+
     def plot_correlation_matrix(self, numerical_features):
+        """Plots a heatmap of the correlation matrix for specified numerical features.
+
+        Args:
+            numerical_features (list): List of numerical feature names to include in the correlation matrix.
+        """
         corr_matrix = self.data[numerical_features].corr()
         sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f")
         plt.title('Correlation Matrix')
         plt.show()
         logg.info("Correlation matrix plotted successfully!")
-        
+
     def plot_distribution_with_class(self, features: list, target: str, feature_type: str = 'numerical'):
-        """Plot the distribution of features with respect to the target class.
+        """Plots the distribution of specified features with respect to the target class.
 
         Args:
             features (list): List of features to plot.
             target (str): The target variable (class).
-            feature_type (str): 'numerical' for histograms and 'categorical' for bar plots. Defaults to 'numerical'.
+            feature_type (str): Type of features ('numerical' or 'categorical'). Defaults to 'numerical'.
         """
         try:
             num_features = len(features)
-            num_cols = min(num_features, 2)  # Up to 3 columns per row
-            num_rows = (num_features + num_cols - 1) // num_cols  # Calculate rows dynamically
+            num_cols = min(num_features, 2)
+            num_rows = (num_features + num_cols - 1) // num_cols
 
             plt.figure(figsize=(num_cols * 5, num_rows * 4))
 
             for i, feature in enumerate(features, 1):
                 if feature not in self.data.columns or target not in self.data.columns:
                     logg.error(f"Feature '{feature}' or target '{target}' not found in data!")
-                    continue  # Skip invalid features or targets
+                    continue
 
                 plt.subplot(num_rows, num_cols, i)
 
                 if feature_type == 'numerical':
-                    # Plot histogram with KDE for each class
                     sns.histplot(data=self.data, x=feature, hue=target, bins=20, kde=True, palette="viridis", element="step")
                     plt.title(f'Distribution of {feature} by {target}')
                     plt.xlabel(feature)
                     plt.ylabel('Frequency')
 
                 elif feature_type == 'categorical':
-                    # Plot countplot for each class
                     sns.countplot(x=feature, hue=target, data=self.data, palette="viridis")
                     plt.title(f'Distribution of {feature} by {target}')
                     plt.xlabel(feature)
